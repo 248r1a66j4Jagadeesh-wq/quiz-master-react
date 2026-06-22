e1f9793e3bd57e248ddf3d70e5cde4af4292e0b8
248r1a66j4Jagadeesh-wq <248r1a66j4@gmail.com>
2026-06-22 21:36:46 +0530

    Added question navigation with Next button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
+
 import "./styles/App.css";
 
 import Header from "./components/Header";
-
 import Question from "./components/Question";
 
 import questions from "./data/questions";
 
 function App() {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
   return (
 
@@ -15,7 +18,15 @@ function App() {
       <Header />
 
       <Question
-        question={questions[0]}
+
+        question={questions[currentQuestion]}
+
+        currentQuestion={currentQuestion}
+
+        setCurrentQuestion={setCurrentQuestion}
+
+        totalQuestions={questions.length}
+
       />
 
     </div>
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,26 +1,85 @@
-import "../styles/Question.css";
 import { useState } from "react";
+
+import "../styles/Question.css";
+
 import Options from "./Options";
 
-function Question({ question }) {
+function Question({
+
+  question,
+
+  currentQuestion,
+
+  setCurrentQuestion,
+
+  totalQuestions
+
+}) {
 
   const [selectedOption, setSelectedOption] = useState("");
 
+  function handleNext() {
+
+    if (selectedOption === "") {
+
+      alert("Please select an answer!");
+
+      return;
+
+    }
+
+    if (currentQuestion < totalQuestions - 1) {
+
+      setCurrentQuestion(currentQuestion + 1);
+
+      setSelectedOption("");
+
+    }
+
+  }
+
   return (
+
     <div className="question">
 
-      <h2>Question {question.id}</h2>
+      <h2>
 
-      <h3>{question.question}</h3>
+        Question {question.id}
+
+      </h2>
+
+      <h3>
+
+        {question.question}
+
+      </h3>
 
       <Options
+
         options={question.options}
+
         selectedOption={selectedOption}
+
         setSelectedOption={setSelectedOption}
+
       />
 
+      <button
+
+        className="next-btn"
+
+        onClick={handleNext}
+
+      >
+
+        Next
+
+      </button>
+
     </div>
+
   );
+
 }
 
 export default Question;
