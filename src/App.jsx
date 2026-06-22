964bfd944c6e2e0d8e7acdf1d14ec1343f18d7d6
248r1a66j4Jagadeesh-wq <248r1a66j4@gmail.com>
2026-06-22 20:30:21 +0530

    Initial React Quiz Master project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>🧠 Quiz Master</h1>
+      <p>Test your React knowledge!</p>
+    </header>
+  );
+}
+
+export default Header;
