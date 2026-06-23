e55f3a435339b9cb1d2d28c955415b831f416b7d
248r1a66j4Jagadeesh-wq <248r1a66j4@gmail.com>
2026-06-23 17:47:51 +0530

    Completed Quiz Master React Project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,13 @@
 import "../styles/Footer.css";
+
 function Footer() {
   return (
     <footer>
       <p>
-        Developed by Jagadeesh using React
+        Developed by Jagadeesh ❤️ using React
       </p>
     </footer>
   );
 }
-footer{
 
-margin-top:40px;
-
-padding:15px;
-
-text-align:center;
-
-color:gray;
-
-font-size:15px;
-
-}
 export default Footer;
